5596269a57a720340fbc36da60cb958c6c40dbdd
Gabriel Melo <gabrielmeloarteaga@gmail.com>
2014-11-19 23:28:38 -0500

    primer commit

---
+++ router.js
@@ -0,0 +1,16 @@
+function route(handle, pathname,response){
+
+	console.log("About to route a request for " + pathname);
+	
+
+	if(typeof handle[pathname] === 'function') {
+		return handle[pathname](response);
+	} else {
+		console.log("No Request handler found for " + pathname);
+		response.writeHead(404,{"Content-Type": "text/plain"});
+		response.write("404 not found");
+		response.end();
+	}
+}
+
+exports.route = route;
